117fb9e78b3eb785e38b53f38b77c279123add41
ND_CANH\ducca <duccanh95hptl@gmail.com>
2023-01-14 09:45:23 +0700

    model auth

--- src/main/java/com/example/backend/BackendApplication.java
+++ src/main/java/com/example/backend/BackendApplication.java
@@ -2,10 +2,12 @@ package com.example.backend;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 
-@SpringBootApplication
+
+@SpringBootApplication()
 public class BackendApplication {
-
+//	exclude={SecurityAutoConfiguration.class}
 	public static void main(String[] args) {
 		SpringApplication.run(BackendApplication.class, args);
 	}
